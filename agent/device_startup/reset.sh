6aad8fef2ac328b62f629c8026db8f9c5dd1ffc3
typemismatch <craig@ip80.com>
2017-08-21 17:39:02 -0400

    added removing gg

--- agent/device_startup/reset.sh
+++ agent/device_startup/reset.sh
@@ -13,6 +13,10 @@ shopt -u extglob
 #Cleanup node-red
 rm /home/node-red/.node-red/*.json
 
+#Cleanup greengrass
+rm -fR /greengrass
+rm -fR /greengrass/configuration/certs
+
 #Copy back defaults
 cp /home/aws/gateway-setup/conf_files/node-red/flows_ip.json /home/node-red/.node-red/flows_$HOSTNAME.json
 cp /home/aws/gateway-setup/agent/device_startup/rootCA.pem /home/aws/rootCA.pem
